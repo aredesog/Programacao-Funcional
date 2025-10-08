073962630ca57f77ddcb55d571c8fe89be113891
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-10-08 18:48:00 -0300

    Add aula02.hs with initial content on Haskell lists and basic functions

---
+++ aula02.hs
@@ -0,0 +1,17 @@
+--LISTAS EM HASKELL (EXECUTAR NO GHCI)
+
+-- ['h','a','s','k','e','l','l'] == "haskell" (DIz se a lista está correta em relação a atribuição (TRUE))      
+
+-- "Hello" ++ " " ++ ['h','a','s','k','e','l','l'] (Adiciona uma string a string anterior)
+-- 1:[2,3,4]  (Adiciona um valor a lista string)
+
+-- Comparação [3,2,1] > [2,1,0] (TRUE) (<,>,=)
+-- [3,2,1] > [2,10,100] (TRUE)
+
+
+--Funções Basicas
+    --head[5,4,3,2,1] -> 5 (Primeiro elemento da lista)
+    --tail[5,4,3,2,1] -> [4,3,2,1] (Todos menos o primeiro)
+    --init[5,4,3,2,1] -> [5,4,3,2] (Todos menos o ultimo)
+    --last[5,4,3,2,1] -> 1 (Ultimo elemento da lista)
+
