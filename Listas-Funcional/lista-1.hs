84915643edc0781cbdcffd5580902f503110f285
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-10-20 17:57:48 -0300

    Remove aula01.hs and aula02.hs files as part of project restructuring

---
+++ Listas-Funcional/lista-1.hs
@@ -0,0 +1,11 @@
+--Implemente uma função meuAbs que devolve o valor absoluto sem usar abs
+
+meuAbs x = if x < 0 else x
+
+--Crie uma função que receba dois números e devolva o maior deles
+
+maior x y = if x > y
+    then "O primeiro e maior"
+    else "O segundo e maior"
+
+--Escreva uma função que diga se um número é positivo, negativo ou zero.
