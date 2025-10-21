7ceb3d6141b7963db644422cc960e74fb37f191a
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-10-21 15:49:18 -0300

    Refatorar funções em lista-1.hs para corrigir formatação e adicionar lógica de cálculo

--- Listas-Funcional/lista-1.hs
+++ Listas-Funcional/lista-1.hs
@@ -1,11 +1,49 @@
 --Implemente uma função meuAbs que devolve o valor absoluto sem usar abs
-
-meuAbs x = if x < 0 else x
+    meuAbs x = if x < 0
+        then -x
+        else x
 
 --Crie uma função que receba dois números e devolva o maior deles
-
-maior x y = if x > y
-    then "O primeiro e maior"
-    else "O segundo e maior"
+    maiorDeDois x y = if x > y
+        then x
+        else y
 
 --Escreva uma função que diga se um número é positivo, negativo ou zero.
+    positivo x = if x>0
+        then "Positivo"
+        else if x<0
+            then "Negativo"
+            else "Zero"
+
+--Crie uma funcao que classifique uam nota de 0 a 10 em conceitos (Excelente, Bom, Regular, Ruim)
+    classificarnota x = if x >= 9.5
+        then "Excelente"
+        else if x >= 7.0
+            then "Bom"
+            else if x >= 5.5
+                then "Regular"
+                else "Ruim"
+
+--Escreva funcoes que retornem o maior e o menor de tres números
+    maior x y z = if x > y && x > z
+        then x
+        else if y > x && y > z
+            then y
+            else z
+
+    menor x y z = if x < y && x < z
+        then x
+        else if y < x && y < z
+            then y
+            else z
+
+--Crie uma calculadora simples que receba dois números e uma operação (+, -, *, /) e devolva o resultado da operação
+    calculadora x y operacao = if operacao == "+"
+        then x + y
+        else if operacao == "-"
+            then x - y
+            else if operacao == "*"
+                then x * y
+                else if operacao == "/"
+                    then x / y
+                    else error "Operação inválida"
