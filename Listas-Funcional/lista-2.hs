2487073f0de750dcd8999a038381e1d93fb49687
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-10-22 13:46:48 -0300

    Add initial implementation of odd number generator and infinite character sequence in lista-2.hs

---
+++ Listas-Funcional/lista-2.hs
@@ -0,0 +1,5 @@
+--Gere os primeiros numeros impares a partir de 1
+    impares lista = [x | x <- lista, mod x 2 /= 0]
+
+--Crie uma sequencia infinita de um caractere c e devolda os primeiros n
+    
