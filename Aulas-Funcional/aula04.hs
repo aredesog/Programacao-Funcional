1caef8acd6c3d6e71ba5cd8c3dbbc4e1f4885a54
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-10-22 18:48:18 -0300

    Add initial implementation of functional programming concepts in aula04.hs

---
+++ Aulas-Funcional/aula04.hs
@@ -0,0 +1,41 @@
+--Tipos de Programacao Funcional
+    {-
+    -> As linguagens funcionais modernas permite que o compilador verifique com mais constancia e segurança.
+        -> Fortemente Tipada ( Mais segurança e previsibilidade)
+        -> Tipagem Estática ( Erros detectados precocemente)
+        -> Inferência de tipos ( não precisa declarar o tipo o tempo todo)
+        -> Polimorfismo parametrico ( reultilização, generalização)
+        -> Classes de tipos ( Permite criar tipos próprios)
+        -> Imutabilidade ( evita efeito colaterais, facilita paralelismo sem conflitos de estados)
+
+
+    -}
+        
+        
+    somaCincoValores:: Int -> Int -> Int -> Int -> Int
+    somCincoValores a b c d e = [1,2,3,4,5]
+
+    saudar:: String -> String
+    saudar a = "Ola," ++ a ++ "!"
+
+    avaliarNOta:: INt -> String
+    avaliarNOta 10 = "Escelente"
+    avaliarNOta 7 = "Boa"
+    avaliarNOta 5 = "Regular"
+    avaliarNOta x = "Insuficiente"
+
+    pegarTerceiro:: (a,b,c) -> c
+    pegarTerceiro (_, _, x) = x
+
+    retirarPrimeiro:: Int-> Int
+    retirarPrimeiro (cabeca:cauda) = cabeca
+
+    --Guardas (Guardas tornam o código mais limpo do que usar múltiplos IFs)
+
+    classifica :: Int -> String
+    classifica x
+        | x < 0 = "Negativo"
+        | x == 0 = "Zero"
+        | otherwise = "Positivo"
+
+    --
