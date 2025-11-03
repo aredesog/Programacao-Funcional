75bee6e1afa7a232f3a80de6a3596ab3473e9c9e
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2025-11-03 15:25:04 -0300

    Refatorar funções em lista-2.hs e lista-3.hs para melhorar legibilidade e consistência

--- Listas-Funcional/lista-2.hs
+++ Listas-Funcional/lista-2.hs
@@ -1,147 +1,66 @@
---1. Gere os primeiros n números ímpares a partir de 1.
---Exemplo:
---primeirosImpares 6 → [1,3,5,7,9,11]
---primeirosImpares 1 → [1]
-primeirosImpares :: Int -> [Int]
-primeirosImpares n = [2*x + 1 | x <- [0..n-1]]
+-- Gere os primeiros n números ímpares a partir de 1.
+    primeirosImpares x = take x [n | n <- [1..], mod n 2 /= 0]
 
---2. Crie uma sequência infinita de um caractere c e devolva os primeiros n caracteres.
---Exemplo:
---repeteChar 6 'x' → "xxxxxx"
----repeteChar 3 'A' → "AAA"
-repeteChar :: Int -> Char -> [Char]
-repeteChar x y = take x (repeat y)  
+-- Crie uma sequência infinita de um caractere c e devolva os primeiros n caracteres.
+    repeticaoChar c n = take n (repeat c)
 
---3. Defina a função oDoMeio3 que, dados 3 inteiros, devolve o inteiro do meio (ou seja, a mediana).
---Exemplo:
---oDoMeio3 4 2 7 = 4
---oDoMeio3 2 (-1) 10 = 2
-oDoMeio3 :: Int -> Int -> Int -> Int
-minVal :: Int -> Int -> Int -> Int
-maxVal :: Int -> Int -> Int -> Int
-minVal x y z =  minimum [x,y,z]
-maxVal x y z = maximum [x,y,z]
-oDoMeio3 x y z = head [n | n <- [x,y,z], n /= minVal x y z, n /= maxVal x y z]
+-- Defina a função oDoMeio3 que, dados 3 inteiros, devolve o inteiro do meio (ou seja, a mediana).
+    minVal x y z =  minimum [x,y,z]
+    maxVal x y z = maximum [x,y,z]
+    oDoMeio3 x y z = head [n | n <- [x,y,z], n /= minVal x y z, n /= maxVal x y z]
 
+-- Defina a função quantosAcimaDaMedia que, dados 3 inteiros, retorna o
+    media x y z =  (x + y + z) `div` 3
+    quantosAcimaDaMedia x y z = length [n | n <- [x,y,z], n > media x y z]
 
---4. Defina a função quantosAcimaDaMedia que, dados 3 inteiros, retorna o
---número de inteiros acima da média.
---Exemplo:
---quantosAcimaDaMedia 1 2 3 = 1
---quantosAcimaDaMedia 4 9 12 = 2
-quantosAcimaDaMedia :: Int -> Int -> Int -> Int
-media :: Int -> Int -> Int -> Int
-media x y z =  (x + y + z) `div` 3
-quantosAcimaDaMedia x y z = length [n | n <- [x,y,z], n > media x y z]
+-- Defina a função numero2 que, dados dois caracteres, retorna um inteiro em
+    numero2 x y
+        | x `elem` ['0'..'9'] && y `elem` ['0'..'9'] = read [x, y]
+        | otherwise = 0
 
---5. Defina a função numero2 que, dados dois caracteres, retorna um inteiro em
---que o algarismo das dezenas é o primeiro carácter e o das unidades é o
---segundo, ou retorna zero se algum dos caracteres não for um dígito.
---Exemplo:
---numero2 '2' '3' = 23
---numero2 '4' '1' = 41
---numero2 :: Int
-numero2 :: Char -> Char -> Int
-numero2 x y
-    | x `elem` ['0'..'9'] && y `elem` ['0'..'9'] = read [x, y]
-    | otherwise = 0
+-- Defina a função anoBissexto que, dado um ano (um inteiro), devolve True se
+    anoBissexto x 
+        | mod x 400 == 0  && mod x 100 /= 0 = True
+        | mod x 4 == 0  && mod x 100 /= 0 = True
+        | otherwise = False
 
---6. Defina a função anoBissexto que, dado um ano (um inteiro), devolve True se
---o ano é bissexto, False se não. (Nota: um ano bissexto é divisível por 400 ou
---é divisível por 4 mas não é divisível por 100)
-anoBissexto :: Int -> Bool
-anoBissexto x 
-    | mod x 400 == 0  && mod x 100 /= 0 = True
-    | mod x 4 == 0  && mod x 100 /= 0 = True
-    | otherwise = False
+-- Crie uma função que divida uma lista ao meio e devolva as duas partes (como
+    quant lista = length lista
+    metade lista 
+        | mod (quant lista) 2 == 0 = [take (quant lista `div` 2) lista, drop (quant lista `div` 2) lista]
+        | otherwise = [take ((quant lista `div` 2) + 1) lista, drop ((quant lista `div` 2) + 1) lista]
 
---7. Crie uma função que divida uma lista ao meio e devolva as duas partes (como
---duas listas dentro de uma lista). Caso o número de elementos seja ímpar, a
---primeira parte deve ter um a mais.
---Exemplo:
---metade [1,2,3,4] → [[1,2],[3,4]]
---metade [10,20,30,40,50] → [[10,20,30],[40,50]]
-quant :: [Int] -> Int
-quant lista = length lista
-metade :: [Int] -> [[Int]]
-metade lista 
-    | mod (quant lista) 2 == 0 = [take (quant lista `div` 2) lista, drop (quant lista `div` 2) lista]
-    | otherwise = [take ((quant lista `div` 2) + 1) lista, drop ((quant lista `div` 2) + 1) lista]
+-- Crie uma função que elimine o primeiro e o último elemento de uma lista.
+    semExtremos lista
+        | length lista < 2 = []
+        | otherwise = init (tail lista)
 
---8. Crie uma função que elimine o primeiro e o último elemento de uma lista.
---Se tiver menos de dois elementos, devolva uma lista vazia.
---Exemplo:
---semExtremos [1,2,3,4,5] → [2,3,4]
---semExtremos [9] → []
-semExtremos :: [Int] -> [Int]
-semExtremos lista
-    | length lista < 2 = []
-    | otherwise = init (tail lista)
+-- Dadas duas extremidades, produza a lista com todos os valores entre elas,
+    intervalo x y
+        | x <= y = enumFromTo x y --[x..y]
+        | otherwise = [x,x-1..y]
 
---9. Dadas duas extremidades, produza a lista com todos os valores entre elas,
---incluindo as extremidades.
---Exemplo:
---intervalo 5 9 → [5,6,7,8,9]
---intervalo 9 5 → [9,8,7,6,5]
-intervalo :: Int -> Int -> [Int]
-intervalo x y
-    | x <= y = enumFromTo x y --[x..y]
-    | otherwise = [x,x-1..y]
-
---10.Dada uma lista, divida-a ao meio e inverta a ordem das duas metades.
---Se o número de elementos for ímpar, o elemento central fica na segunda
---metade.
---Exemplo:
---inverteMetades [1,2,3,4] → [3,4,1,2]
---inverteMetades [10,20,30,40,50] → [30,40,50,10,20]
-quantMet :: [Int] -> Int
-quantMet lista = length lista
-metadeLista :: [Int] -> [Int]
-metadeLista lista  = drop (quant lista `div` 2) lista ++ take (quant lista `div` 2) lista
+--Dada uma lista, divida-a ao meio e inverta a ordem das duas metades.
+    quantMet lista = length lista
+    metadeLista lista  = drop (quant lista `div` 2) lista ++ take (quant lista `div` 2) lista
 
 
---11. Defina uma função que insere um elemento em uma posição dada,
---deslocando os demais à direita. Se a posição for maior que o tamanho, insira
---no final.
---Exemplo:
---inserePos 2 99 [10,20,30,40] → [10,20,99,30,40]
---inserePos 10 5 [1,2,3] → [1,2,3,5]
-inserePos :: Int -> Int -> [Int] -> [Int]
-inserePos pos elemento lista = take pos lista ++ [elemento] ++ drop pos lista
+-- Defina uma função que insere um elemento em uma posição dada,
+    inserePos pos elemento lista = take pos lista ++ [elemento] ++ drop pos lista
 
---12.Defina uma função que dado um inteiro troca o algarismo das unidades com o
---das dezenas.
---Exemplo:
---trocaPosicao 123 = 132
---trocaPosicao :: Int -> Int
-trocaPosicao :: Int -> Int
-trocaPosicao x = resto * 100 + u * 10 + d
-  where
-    u = x `mod` 10
-    d = (x `div` 10) `mod` 10
-    resto = x `div` 100
+-- Defina uma função que dado um inteiro troca o algarismo das unidades com o
+    trocaPosicao x = resto * 100 + u * 10 + d
+        where
+            u = x `mod` 10
+            d = (x `div` 10) `mod` 10
+            resto = x `div` 100
 
--- 13 -Escreva, em Haskell, a função invertInt::Int->Int que inverta os dígitos de um
---número inteiro.
---Exemplo: invertInt 123 = 321.
-invertInt :: Int -> Int
-invertInt x = read (reverse (show x)) :: Int
+-- -Escreva, em Haskell, a função invertInt::Int->Int que inverta os dígitos de um
+    invertInt x = read (reverse (show x)) :: Int
 
 
---14- Crie uma função que determine se um número é palíndromo (igual quando invertido).
---Exemplo:
---palindromo 121 → True
---palindromo 123 → False
---palindromo 1001 → True
-palindromo :: Int -> Bool
-palindromo n = n == (read (reverse (show n)) :: Int)
+-- Crie uma função que determine se um número é palíndromo (igual quando invertido).
+    palindromo n = n == (read (reverse (show n)) :: Int)
 
---15.Defina a função dezenas que, dado um inteiro, retorna o dígito das dezenas.
---Exemplo:
---dezenas 1234 → 3
---dezenas 507 → 0
---dezenas 9 → 0
-dezenas :: Int -> Int
-dezenas x = (x `div` 10) `mod` 10
-
---16 é igual a 12
+-- Defina a função dezenas que, dado um inteiro, retorna o dígito das dezenas.
+    dezenas x = (x `div` 10) `mod` 10
--- Listas-Funcional/lista-3.hs
+++ Listas-Funcional/lista-3.hs
@@ -1,102 +1,43 @@
 --Crie uma função que inverta os elementos de uma tupla 
---Exemplo:
---troca (1,2) → (2,1)
-troca :: (x,y) -> (y,x)
-troca (x, y) = (y,x)
+    troca (x, y) = (y,x)
 
 ---Crie uma função que recebe duas tuplas, cada uma contendo o nome de uma
---disciplina e a nota do aluno nessa disciplina. A função deve retornar:
---a. "materias diferentes" se os nomes das disciplinas não forem iguais;
---b. "Aprovado" se as matérias forem iguais e a média aritmética das duas
---notas for maior ou igual a 6;
---c. "Reprovado" caso contrário.
-media :: (String, Float) -> (String, Float) -> Float
-media (m1, n1) (m2, n2) = (n1+n2)/2
-materias :: (String, Float) -> (String, Float) -> String
-materias (m1, n1) (m2, n2)
-    | m1 /= m2  = "materias diferentes"
-    | media (m1, n1) (m2, n2) >= 6.0 = "Aprovado"
-    | otherwise = "Reprovado"
+    media (m1, n1) (m2, n2) = (n1+n2)/2
+    materias (m1, n1) (m2, n2)
+        | m1 /= m2  = "materias diferentes"
+        | media (m1, n1) (m2, n2) >= 6.0 = "Aprovado"
+        | otherwise = "Reprovado"
     
 --Dada uma lista de tuplas [(Nome, Idade)], defina a função maioresDeIdade
---que retorna uma lista com os nomes (somente as strings) das pessoas com
---18 anos ou mais.
---Exemplo:
---maioresDeIdade [("Ana", 17), ("Beto", 20)] => ["Beto"]
-maioresDeIdade :: [(String, Int)] -> [String]
-maioresDeIdade pessoas = [ nome | (nome, idade) <- pessoas, idade >= 18]
+    maioresDeIdade pessoas = [ nome | (nome, idade) <- pessoas, idade >= 18]
 
 --Crie uma função que recebe duas horas (hora, minuto) e devolve a diferença
---entre elas.
---Exemplo:
---diferencaHoras (14, 30) (12, 45) -> (1,45)
-diferencaHoras :: (Int, Int) -> (Int, Int) -> (Int, Int)
-diferencaHoras (h1, m1) (h2, m2) = (abs (h1 * 60 + m1 - h2 * 60 - m2) `div` 60, abs (h1 * 60 + m1 - h2 * 60 - m2) `mod` 60)
+    diferencaHoras (h1, m1) (h2, m2) = (abs (h1 * 60 + m1 - h2 * 60 - m2) `div` 60, abs (h1 * 60 + m1 - h2 * 60 - m2) `mod` 60)
 
---Defina a função primos n que retorna todos os números primos até utilizando
---a lógica da Compreensão de Listas, onde um número é primo se não possui
---divisores no intervalo.
---Exemplo:
---primos 10 => [2, 3, 5, 7]
-primos :: Int -> [Int]
-primos n = [x | x <- [0..n], mod x n == 0]
-
---6 é igual a 4
+--Defina a função primos n que retorna todos os números primos até n.
+    primos n = [x | x <- [0..n], mod x n == 0]
 
 --Dada uma lista de (cidade, temperatura), filtre as que estão acima de 30°C.
---Exemplo:
---temperaturasAltas [("Alfenas", 38), ("Alterosa", 5), ("Areado", 40)]->[("Alfenas", 38),("Areado", 40)]
-temp :: [(String, Int)] ->[(String, Int)]
-temp temps = [(x,y) | (x,y) <- temps, y>30]
+    temp temps = [(x,y) | (x,y) <- temps, y>30]
 
 --Utilizando listas em compreensão, defina a função termina_em que recebe
---um número n e devolve a lista com os números entre 0 e 100 que terminam
---com o número n.
---Exemplo:
---termina_em 3 = [3,13,23,33,43,53,63,73,83,93]
-termina_em :: Int -> [Int]
-termina_em num = [x | x <- [0..100], mod x 10 == num]
+    termina_em num = [x | x <- [0..100], mod x 10 == num]
 
 --Defina a função divisivelPor que dado um inteiro (de 0 a 9) retorna uma lista
---com todos os números entre 1 e 100 que são divisíveis pelo número.
---Exemplo:
---divisivel 7 =[7,14,21,28,35,42,49,56,63,70,77,84,91,98]
-divisivelPor :: Int -> [Int]
-divisivelPor num = [x | x <- [1..100], mod x num == 0]
+    divisivelPor num = [x | x <- [1..100], mod x num == 0]
 
 --Sem usar a função length, defina a função comprimento que dada uma lista
---retorna o número de elementos da lista.
---Exemplo:
---comprimento [1,2,1,3] = 4
-comprimento :: [Int] -> Int
-comprimento x = sum [1 | _ <- x]
+    comprimento x = sum [1 | _ <- x]
 
---Defina a função quantos que dada uma lista e um elemento devolve o número
---de vezes que o elemento está na lista.
---Exemplo:
---quantos [3,7,4,3,9,3,0,3,2] 3 = 4
-quantos :: [Int] -> Int -> Int
-quantos lista num = sum [1 | x <- lista, x == num]
+--Defina a função quantos que dada uma lista e um elemento devolve o número de vezes que o elemento está na lista.
+    quantos lista num = sum [1 | x <- lista, x == num]
 
 --Defina a função repetidos que dada uma lista retorna uma lista com os
---elementos que estão repetidos.
---Exemplo:
---repetidos [1,2,1,3,4,3,1] = [1,3]
-conta :: (Eq a) => a -> [a] -> Int
-conta x lista = length [y | y <- lista, y == x]
+    conta x lista = length [y | y <- lista, y == x]
 
-removeDuplicados :: (Eq a) => [a] -> [a]
-removeDuplicados [] = []
-removeDuplicados (x:xs)
-    | x `elem` xs = removeDuplicados xs
-    | otherwise   = x : removeDuplicados xs
+    removeDuplicados [] = []
+    removeDuplicados (x:xs)
+        | x `elem` xs = removeDuplicados xs
+        | otherwise   = x : removeDuplicados xs
 
-repetidos :: (Eq a) => [a] -> [a]
-repetidos xs = removeDuplicados [x | x <- xs, conta x xs > 1]
-
---Defina a função contaQuantosDiferentes que retornam o número de
---elementos diferentes do inteiro na lista.
---Exemplo:
---contaQuantosDiferentes [1,3,4,1] 1 = 2
-contaQuantosDiferentes :: [Int] -> Int -> Int
-contaQuantosDiferentes lista num = sum [1 | x <- lista, x /= num]
+    repetidos xs = removeDuplicados [x | x <- xs, conta x xs > 1]
